2380b7a026a1d89772a0d167967f6245b7be09e0
Aspandiyar933 <ggg.megadoom.lol@gmail.com>
2023-10-17 15:31:34 +0600

    add price

--- addxml.py
+++ addxml.py
@@ -2,10 +2,20 @@ import xml.etree.ElementTree as ET
 import mysql.connector
 from mysql.connector import Error
 from config import *
-import logging 
+import logging
+import os
 
-logging.basicConfig(level=logging.INFO, filename='log.log', filemode='w',
-                    format='%(asctime)s - %(levelname)s - %(message)s)')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+logging.basicConfig(level=logging.INFO, filename=os.path.join(BASE_DIR, '.log'), filemode='w',
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+try:
+    with open(os.path.join(BASE_DIR, 'query.sql'), 'r', encoding='utf-8') as file:
+        query = file.read()
+except FileNotFoundError as err:
+    logging.INFO("File not found", err)
+
 
 try:
     conn = mysql.connector.connect(
@@ -14,32 +24,26 @@ try:
         password=PASSWORD,
         database=DATABASE
     )
+
+    cursor = conn.cursor()
+    cursor.execute(query)
+    data = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
 except Error as err:
-    logging.error("Error while connecting to MySQL", err)
+    logging.error("Error while connecting to MySQL", str(err))
 
-cursor = conn.cursor()
-
-try:
-    with open('query.sql', 'r', encoding='utf-8') as file:
-        query = file.read()
-except FileNotFoundError as err:
-    logging.INFO("File not found", err)
-
-cursor.execute(query)
-
-data = cursor.fetchall()
-
-#tree = ET.parse('newlist.xml')
-#root = tree.getroot()
 
 root = ET.Element("yml_catalog")
 shop = ET.SubElement(root, "shop")
 categories = ET.SubElement(shop, "categories")
 offers = ET.SubElement(shop, "offers")
 
+tree = ET.ElementTree(root)
+
 
 result = {}
-data_about_goods = {}
 counter = 0
 for row in data:
     if not result.get(row[4]) and row[4] != None:
@@ -48,54 +52,47 @@ for row in data:
         category.set("id", str(row[4]))
         category.set('type', 'new')
         category.text = str(row[5])
-        #categories_element = root.find('.//categories')
-        #categories_element.append(category)
-    data_about_goods[row[0]] = [row[1], row[2], row[3], row[4], row[5], row[6], row[7]]
+
     counter += 1
+
     offer = ET.SubElement(offers, "offer")    
     offer.set("id", str(row[0]))
     offer.set("type", "vendor.model")
-    #offers_element = root.find('.//offers')
-    #offers_element.append(offer)
+
     typePrefix = ET.SubElement(offer, "typePrefix")
     typePrefix.text = "Товар"
+
     vendor = ET.SubElement(offer, "vendor")
     vendor.text = "BOSCH"
+
     model = ET.SubElement(offer, "model")
     model.text = str(row[1])
+
     name = ET.SubElement(offer, "name")
     name.text = str(row[2])
+
+    price_or = row[9] or row[8] 
+    price = ET.SubElement(offer, "price")
+    price.text = str(int(price_or))
+
     currencyId = ET.SubElement(offer, "currencyId")
     currencyId.text = "KZT"
+
     categoryId = ET.SubElement(offer, "categoryId")
     categoryId.text = str(row[4])
-    picture = ET.SubElement(offer, "picture")
-    picture.text = str(row[3])
-    url = ET.SubElement(offer, "url")
-    url.text = str(row[7])
-    shortDescription = ET.SubElement(offer, "shortDescription")
-    shortDescription.text = str(row[6])
-    # offer.append(typePrefix)
-    # offer.append(vendor)
-    # offer.append(model)
-    # offer.append(name)
-    # offer.append(currencyId)
-    # offer.append(categoryId)
-    # offer.append(picture)
-    # offer.append(url)
-    # offer.append(shortDescription)
 
-tree = ET.ElementTree(root)
+    picture = ET.SubElement(offer, "picture")
+    picture.text = 'https://boschcenter.kz/%s' % str(row[3])
+
+    url = ET.SubElement(offer, "url")
+    url.text = 'https://boschcenter.kz/%s' % str(row[7])
+
+    shortDescription = ET.SubElement(offer, "shortDescription")
+    shortDescription.text = str(row[6])[:-64] + '.'
 
 try:
-    tree.write('your_modified_xml_file.xml', encoding='utf-8', xml_declaration=True)
+    tree.write(os.path.join(BASE_DIR, 'feed.xml'), encoding='utf-8', xml_declaration=True)
     print(f"New category added successfully. And has adding {counter} goods")
     logging.info(f"New category added successfully. And has adding {counter} goods")
 except:
     logging.error("Error while writing to file")
-
-#print(data_about_goods)
-
-cursor.close()
-conn.close()
-
--- query.sql
+++ query.sql
@@ -1,19 +1,22 @@
 SELECT t1.product_id,
-    model, 
+    model,
     t2.name product_name,
     t1.image picture,
-    t5.category_id, 
-    t5.name category_name, 
-    feed_text description, 
-    keyword url 
-FROM 
-    oc_product t1 
-    LEFT JOIN oc_product_description t2 ON t1.product_id = t2.product_id 
-    LEFT JOIN oc_product_to_category t3 ON t1.product_id = t3.product_id 
-    LEFT JOIN oc_category t4 ON t3.category_id = t4.category_id 
-    LEFT JOIN oc_category_description t5 ON t4.category_id = t5.category_id 
-    LEFT JOIN oc_seo_url t6 ON t1.product_id = SUBSTRING_INDEX(t6.query, '=', -1) 
-WHERE query like 'product_id%' 
-    AND t2.language_id = 1 
-    AND t5.language_id = 1 
+    t5.category_id,
+    t5.name category_name,
+    feed_text description,
+    keyword url,
+    t1.price rrp,
+    t7.price prp
+FROM
+    oc_product t1
+    LEFT JOIN oc_product_description t2 ON t1.product_id = t2.product_id
+    LEFT JOIN oc_product_to_category t3 ON t1.product_id = t3.product_id
+    LEFT JOIN oc_category t4 ON t3.category_id = t4.category_id
+    LEFT JOIN oc_category_description t5 ON t4.category_id = t5.category_id
+    LEFT JOIN oc_seo_url t6 ON t1.product_id = SUBSTRING_INDEX(t6.query, '=', -1)
+    LEFT JOIN oc_product_special t7 ON t1.product_id = t7.product_id
+WHERE query like 'product_id%'
+    AND t2.language_id = 1
+    AND t5.language_id = 1
     and t6.store_id = 3
